c59694ac8292fc1b61877cfaed461471ce7a0ff5
Ryzevv <75268177+Ryzevv@users.noreply.github.com>
2021-05-20 22:34:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+﻿/////////////////////////////////////////////////////
+// VARIABLES
+/////////////////////////////////////////////////////
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const configs = require("./configs.json");
+const fivereborn = require('fivereborn-query');
+client.config = configs;
+
+/////////////////////////////////////////////////////
+// DEMARRER LE BOT
+/////////////////////////////////////////////////////
+
+client.login(configs.token)
+  .then(
+    () => {
+      console.log("Wsh Man ton serveur est bien démarré");
+      console.log("Collecte d'informations en cours... ");
+    },
+    () => {
+      client.destroy();
+      console.log("Bot détruit!");
+    });
+
+function activity() {
+  setTimeout(() => {
+    fivereborn.query(configs.serverInfo[0], configs.serverInfo[1], (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        client.user.setActivity(" Connecté: " + data.clients + "/" + data.maxclients, { type: configs.activityType });
+      }
+    });
+    activity();
+  }, 10000);
+}
+activity();
